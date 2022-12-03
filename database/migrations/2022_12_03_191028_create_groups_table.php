9cff36aef8f9c1f99af3f94ae3f51fe4143694f4
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2022-12-03 19:25:30 +0900

    測定記録テーブル、グループテーブルの作成 #5

---
+++ database/migrations/2022_12_03_191028_create_groups_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('name')->unique();
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->dropForeign('groups_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
+        Schema::dropIfExists('groups');
+    }
+}
