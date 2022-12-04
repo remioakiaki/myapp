00643ba1d683847333a805203889d7bac281ff67
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2022-12-04 16:07:44 +0900

    グループ追加機能の作成 #9

---
+++ resources/views/group/create.blade.php
@@ -0,0 +1,43 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('group.store') }}">
+            @csrf
+
+            <!-- Name -->
+            <div>
+                <x-label for="name" :value="__('Name')" />
+
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            </div>
+
+            <!-- Image -->
+            <div class="mt-4">
+                <x-label for="image" :value="__('Image')" />
+                   <x-input id="image" class="block mt-1 w-full"
+                                type="file"
+                                name="image"
+                                :value="old('image')"
+                                />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-button class="ml-4">
+                    {{ __('Register') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
