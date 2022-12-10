8645915a91cf567cd7b2169c4271befc2dc2c719
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2022-12-10 17:25:53 +0900

    attachした際にcreated_atとupdate_atに時間が追加されるように変更

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,6 @@ class User extends Authenticatable
 
     public function groups()
     {
-        return $this->belongsToMany(Group::class, 'belonging');
+        return $this->belongsToMany(Group::class, 'belonging')->withTimestamps();
     }
 }
