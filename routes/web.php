c5dd1a871b7e32663d7af9a290e463270fd10f4f
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2022-12-11 22:12:55 +0900

    ルーティングの追加。優先順位を考慮して記載の順番の変更#18

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::group(['prefix' => '/user'], function () {
 Route::group(['prefix' => '/group'], function () {
     Route::get('/', [GroupController::class, 'index'])->name('group.index');
     Route::get('/welcome', [GroupController::class, 'welcome'])->name('group.welcome');
-    // Route::get('show/{id}', [GroupController::class, 'show'])->name('group.show');
+    Route::get('show/{id}', [GroupController::class, 'show'])->name('group.show');
     // Route::get('edit/{id}', [GroupController::class, 'edit'])->name('group.edit');
     Route::get('create', [GroupController::class, 'create'])->name('group.create');
     Route::post('store', [GroupController::class, 'store'])->name('group.store');
@@ -54,11 +54,12 @@ Route::group(['prefix' => '/belonging'], function () {
 });
 
 Route::group(['prefix' => '/record'], function () {
-    Route::get('/', [RecordController::class, 'index'])->name('record.index');
+    Route::get('/create', [RecordController::class, 'create'])->name('record.create');
+    Route::get('/{user_id}', [RecordController::class, 'index'])->name('record.index');
     Route::get('/welcome', [RecordController::class, 'welcome'])->name('record.welcome');
     // Route::get('show/{id}', [RecordController::class, 'show'])->name('record.show');
     // Route::get('edit/{id}', [RecordController::class, 'edit'])->name('record.edit');
-    Route::get('create', [RecordController::class, 'create'])->name('record.create');
+
     Route::post('store', [RecordController::class, 'store'])->name('record.store');
     // Route::post('update/{id}', [RecordController::class, 'update'])->name('record.update');
     // Route::post('delete/{id}', [RecordController::class, 'delete'])->name('record.delete');
