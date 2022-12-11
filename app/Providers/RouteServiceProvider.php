2428f6382af57040b8c7f62daa943d6e529c8352
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2022-12-11 14:33:46 +0900

    ログイン後の遷移先の変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/record/create';
 
     /**
      * The controller namespace for the application.
